b401bb65cf88350807691ed82e287eae89961793
HuynhThaiHCMUT <thai.dinhhuynh@hcmut.edu.vn>
2024-11-03 11:36:05 +0700

    remove test data

--- src/components/Requests/RequestList.js
+++ src/components/Requests/RequestList.js
@@ -9,36 +9,7 @@ const RequestList = () => {
     const fetchRequests = async () => {
       try {
         const response = await axios.get('http://localhost:8080/api/requests/');
-        //setRequests(response.data);
-        setRequests([
-          {
-            id: 1,
-            detail: 'Request 1',
-            createdAt: '2021-10-01',
-            user: { name: 'User 1' },
-            deposit: 1000,
-            totalPrice: 5000,
-            status: 'pending',
-          },
-          {
-            id: 2,
-            detail: 'Request 2',
-            createdAt: '2021-10-02',
-            user: { name: 'User 2' },
-            deposit: 2000,
-            totalPrice: 6000,
-            status: 'approved',
-          },
-          {
-            id: 3,
-            detail: 'Request 3',
-            createdAt: '2021-10-03',
-            user: { name: 'User 3' },
-            deposit: 3000,
-            totalPrice: 7000,
-            status: 'rejected',
-          },
-        ]);
+        setRequests(response.data);
       } catch (error) {
         console.error('Error fetching requests', error);
       }
